353b4f7f8e33b99d204bb2777e7cf6761cf78078
Evgeniy KIRICHENKO <ekiriche@e2r5p9.unit.ua>
2018-05-15 20:44:43 +0300

    asd

---
+++ app/Controllers/Auth/AuthController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Controllers\Auth;
+
+use App\Config\db;
+use App\Controllers\Controller;
+use Respect\Validation\Validator as v;
+
+class AuthController extends Controller
+{
+	public function getSignup($request, $response)
+    {
+        return $this->view->render($response, 'auth/signup.twig');
+    }
+    
+    public function postSignup($request, $response)
+    {
+        $validation = $this->validator->validate($request, [
+            'login' => v::noWhitespace()->notEmpty(), 
+            'email' => v::noWhitespace()->notEmpty(), 
+            'password' => v::noWhitespace()->notEmpty(), 
+        ]);
+        
+        if ($validation->failed())
+        {
+           return $response->withRedirect($this->router->pathFor('auth.signup')); 
+        }
+        
+        $db = new db();
+        $db = $db->connect();
+        
+        $stm = $db->prepare("INSERT INTO users (login, email, password) VALUES (?, ?, ?)");
+        $stm->bindParam(1, $request->getParam('login'));
+        $stm->bindParam(2, $request->getParam('email'));
+        $stm->bindParam(3, hash("sha256", $request->getParam('password')));
+        $stm->execute();
+        
+        return $response->withRedirect($this->router->pathFor('home'));
+    }
+}
+
+?>
